74ed49ca827add21ca7f8e6b7262b184181c3fcb
lijnb <lijnbelt13@gmail.com>
2025-05-16 20:10:22 +0200

    feat(quit): set gamemode on quit

--- core/src/main/kotlin/com/undefined/akari/manager/GamemodeManager.kt
+++ core/src/main/kotlin/com/undefined/akari/manager/GamemodeManager.kt
@@ -1,10 +1,26 @@
 package com.undefined.akari.manager
 
+import com.undefined.akari.AkariConfig
+import com.undefined.akari.events.AkariEvent
+import org.bukkit.Bukkit
 import org.bukkit.GameMode
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerQuitEvent
 import java.util.UUID
 
-object GamemodeManager {
+object GamemodeManager : Listener {
 
     val pastGameMode: HashMap<UUID, GameMode> = hashMapOf()
+
+    init {
+        Bukkit.getPluginManager().registerEvents(this, AkariConfig.javaPlugin)
+    }
+
+    @EventHandler
+    fun onQuit(e: PlayerQuitEvent) {
+        pastGameMode[e.player.uniqueId]?.run { e.player.gameMode = this }
+        pastGameMode.remove(e.player.uniqueId)
+    }
 
 }
